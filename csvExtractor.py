c1756ba6b463bf12ed29e190f163ee136dd4a06a
Jorge Sepulveda <gSepulveda9697@gmail.com>
2020-04-11 16:43:25 -0500

    Csv extractor (#1)
    * added us-countydata and csv extractor script for mapping purposes
    
    * added us-countydata and csv extractor script for mapping purposes
    
    * added git ignore
    
    * fixed files
    
    fixed git ignore
    
    removed vscode folder
    
    added states extraction
    
    New data for 4/11.
    
    Fix numbers for 4/11 for Dallas, TX.
    
    New data for 4/12.
    
    Add Georgia to geographic exceptions with explanation of 4/12 case count drop.
    
    New data for 4/13.
    Removed cases from Alachua, FL for 4/12.
    Removed cases from Douglas, NE for 4/11 and 4/12.
    
    New data for 4/14.
    
    Adding a national level us.csv file with case and death counts.
    
    progress bar and file upload
    
    added population 2019 data
    
    Fix D.C. county level for 4/14 to match D.C. state level
    
    New data for 4/15.
    
    updated and with pops

---
+++ csvExtractor.py
@@ -0,0 +1,99 @@
+import csv
+import pandas as pd
+import json
+from tqdm import tqdm
+
+def extractCSVpop():
+    print('extracting the population CSV')
+    fields = []
+    rows = []
+    popCounties = {}
+    with open('co-est2019-alldata.csv', 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        fields = next(csvreader)
+        for row in list(csvreader):
+            rows.append(row) 
+        # get total number of rows 
+        #print("Total no. of rows: %d"%(csvreader.line_num))
+    #print('Field names are:' + ', '.join(field for field in fields))
+    # parsing each column of a row 
+    for row in rows: 
+        fips = ("%02d" % int(row[3]) + "%03d" % int(row[4]))
+        pop2019 = row[18]
+        popCounties[fips] = {}
+        popCounties[fips]['POPESTIMATE2019'] = row[18]
+    return popCounties
+
+def extractCSV():
+    print('extracting the counties CSV')
+    fields = []
+    rows = []
+    affectedCounties = []
+    affectedCountyData = []
+    with open('us-counties.csv', 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        fields = next(csvreader)
+        for row in reversed(list(csvreader)):
+            rows.append(row) 
+        # get total number of rows 
+        print("Total no. of rows: %d"%(csvreader.line_num))
+    #print('Field names are:' + ', '.join(field for field in fields))
+    # parsing each column of a row 
+    for row in rows: 
+        if row[3] not in affectedCounties:
+            affectedCounties.append(row[3])
+            affectedCountyData.append([row[1],row[2],row[3],row[4],row[5]])
+    return affectedCountyData
+
+def convertToJson(affectedCountyData):
+    populationData = extractCSVpop()
+    print('converting CSV to JSON')
+    affectedCountiesJ = {}
+    for countyData in affectedCountyData:
+        try:
+            fipsCode = str(countyData[2])
+            affectedCountiesJ[fipsCode] = {}
+            affectedCountiesJ[fipsCode]['county'] = countyData[0]
+            affectedCountiesJ[fipsCode]['state'] = countyData[1]
+            affectedCountiesJ[fipsCode]['fips'] = countyData[2]
+            affectedCountiesJ[fipsCode]['cases'] = countyData[3]
+            affectedCountiesJ[fipsCode]['deaths'] = countyData[4]
+            affectedCountiesJ[fipsCode]['POPESTIMATE2019'] = populationData[fipsCode]['POPESTIMATE2019']
+        except Exception:
+            print(fipsCode)
+            pass
+    return affectedCountiesJ
+
+def mergeData(affectedCountiesJ):
+    popData = extractCSVpop()
+    print('merging da data')
+    counties = {}
+    with open('counties.json', 'r') as file:
+        counties = json.load(file)
+        #print(len(counties['features']))
+    for i in tqdm(range(len(counties['features']))):
+        try:
+            fipsCode = counties['features'][i]['properties']['STATE'] + counties['features'][i]['properties']['COUNTY']
+            counties['features'][i]['properties']['cases'] = int(affectedCountiesJ[fipsCode]['cases'])
+            counties['features'][i]['properties']['deaths'] = int(affectedCountiesJ[fipsCode]['deaths'])
+            counties['features'][i]['properties']['POPESTIMATE2019'] = int(affectedCountiesJ[fipsCode]['POPESTIMATE2019'])
+            #print(counties['features'][i]['properties'])
+        except Exception:
+            counties['features'][i]['properties']['cases']=0
+            counties['features'][i]['properties']['deaths']=0
+            counties['features'][i]['properties']['POPESTIMATE2019'] = 1
+            #print(str(counties['features'][i]['properties']) + "zeroed")
+    return counties
+
+def saveFiles(newCountyData, affectedCounties):
+    print('Saving Files')
+    with open('countyData.json', 'w') as f:
+        json.dump(affectedCounties, f, indent=2)
+
+    with open('counties-with-cases.json', 'w') as f:
+        json.dump(newCountyData, f, separators=(',', ':'))
+
+countyArrays = extractCSV()
+affectedCountyJson = convertToJson(countyArrays)
+updatedCountyData = mergeData(affectedCountyJson)
+saveFiles(updatedCountyData, affectedCountyJson)
