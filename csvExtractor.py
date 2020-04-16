ea62a73dc10172f60acb586b9194c364e0c8fa5c
Jorge Sepulveda <gSepulveda96@tamu.edu>
2020-04-16 12:08:24 -0500

    added population 2019 data

--- csvExtractor.py
+++ csvExtractor.py
@@ -3,6 +3,27 @@ import pandas as pd
 import json
 from tqdm import tqdm
 
+def extractCSVpop():
+    print('extracting the population CSV')
+    fields = []
+    rows = []
+    popCounties = {}
+    with open('co-est2019-alldata.csv', 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        fields = next(csvreader)
+        for row in list(csvreader):
+            rows.append(row) 
+        # get total number of rows 
+        #print("Total no. of rows: %d"%(csvreader.line_num))
+    #print('Field names are:' + ', '.join(field for field in fields))
+    # parsing each column of a row 
+    for row in rows: 
+        fips = ("%02d" % int(row[3]) + "%03d" % int(row[4]))
+        pop2019 = row[18]
+        popCounties[fips] = {}
+        popCounties[fips]['POPESTIMATE2019'] = row[18]
+    return popCounties
+
 def extractCSV():
     print('extracting the counties CSV')
     fields = []
@@ -25,15 +46,22 @@ def extractCSV():
     return affectedCountyData
 
 def convertToJson(affectedCountyData):
+    populationData = extractCSVpop()
     print('converting CSV to JSON')
     affectedCountiesJ = {}
     for countyData in affectedCountyData:
-        affectedCountiesJ[str(countyData[2])] = {}
-        affectedCountiesJ[str(countyData[2])]['county'] = countyData[0]
-        affectedCountiesJ[str(countyData[2])]['state'] = countyData[1]
-        affectedCountiesJ[str(countyData[2])]['fips'] = countyData[2]
-        affectedCountiesJ[str(countyData[2])]['cases'] = countyData[3]
-        affectedCountiesJ[str(countyData[2])]['deaths'] = countyData[4]
+        try:
+            fipsCode = str(countyData[2])
+            affectedCountiesJ[fipsCode] = {}
+            affectedCountiesJ[fipsCode]['county'] = countyData[0]
+            affectedCountiesJ[fipsCode]['state'] = countyData[1]
+            affectedCountiesJ[fipsCode]['fips'] = countyData[2]
+            affectedCountiesJ[fipsCode]['cases'] = countyData[3]
+            affectedCountiesJ[fipsCode]['deaths'] = countyData[4]
+            affectedCountiesJ[fipsCode]['POPESTIMATE2019'] = populationData[fipsCode]['POPESTIMATE2019']
+        except Exception:
+            print(fipsCode)
+            pass
     return affectedCountiesJ
 
 def mergeData(affectedCountiesJ):
